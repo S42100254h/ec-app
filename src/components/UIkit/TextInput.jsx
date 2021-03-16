9fb648766592f34794378aa8105d5737ca0401b1
yuki sawabe <sawabe0v0@gmail.com>
2021-03-16 16:31:36 +0900

    fix compile error

--- src/components/UIkit/TextInput.jsx
+++ src/components/UIkit/TextInput.jsx
@@ -1,10 +1,10 @@
 import React from "react";
-import TextField from "@material-ui/core";
+import TextField from "@material-ui/core/TextField";
 
 const TextInput = (props) => {
   return (
     <TextField
-      fullwidth={props.fullwidth}
+      fullWidth={props.fullWidth}
       label={props.label}
       margin="dense"
       multiline={props.multiline}
