1304b1c5b44e34ed64e9a57dd9aaa66e639e7bc2
yuki sawabe <sawabe0v0@gmail.com>
2021-03-16 10:24:21 +0900

    fix compile error

--- src/index.js
+++ src/index.js
@@ -2,9 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from "react-redux";
 import createStore from "./reducks/store/store";
-import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 
 export const store = createStore();
 
@@ -18,4 +16,3 @@ ReactDOM.render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
