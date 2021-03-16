48b7952b9a1821368922d0d73ab504904b1ba3a5
yuki sawabe <sawabe0v0@gmail.com>
2021-03-16 12:16:07 +0900

    create operation.js

---
+++ src/reducks/users/operations.js
@@ -0,0 +1,18 @@
+import { signInAction } from "./actions";
+import { push } from "connected-react-router";
+
+export const signIn = (email, password) => {
+  return async (dispatch, getState) => {
+    const state = getState();
+    const isSignedIn = state.users.isSignedIn;
+
+    if (!isSignedIn) {
+      const userDate = await emailSignIn(email, password)
+      dispatch(signInAction({
+        isSignedIn: true,
+        uid: "0001",
+        username: "neko"
+      }));
+    }
+  };
+};
