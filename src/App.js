088b8d71fd187b84100dae449b4f3b69cd4d5db7
yuki sawabe <sawabe0v0@gmail.com>
2021-03-16 10:48:59 +0900

    modify App.js

--- src/App.js
+++ src/App.js
@@ -1,21 +1,10 @@
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import React from "react";
+import Router from "./Router";
 
-export default App;
+const App = () => {
+  return(
+    <main>
+      <Router />
+    </main>
+  );
+};
