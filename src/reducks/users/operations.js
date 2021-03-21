8bc1850b935e0ebcda801fe2f35d8386b1782008
yuki sawabe <sawabe0v0@gmail.com>
2021-03-21 11:13:46 +0900

    add validate common method to operations

--- src/reducks/users/operations.js
+++ src/reducks/users/operations.js
@@ -1,12 +1,18 @@
 import { signInAction } from "./actions";
 import { push } from "connected-react-router";
 import { auth, db, FirebaseTimeStamp } from "../../firebase/index";
+import { isValidEmailFormat, isValidRequiredInput } from "../../function/common";
 
 export const signIn = (email, password) => {
   return async (dispatch) => {
     // Validation
-    if (email === "" || password === "") {
-      alert("必須項目が未入力です");
+    if (!isValidRequiredInput(email, password)) {
+      alert("メールアドレスかパスワードが未入力です。");
+      return false;
+    }
+
+    if (!isValidEmailFormat(email)) {
+      alert("メールアドレスの形式が不正です");
       return false;
     }
 
@@ -43,13 +49,18 @@ export const signIn = (email, password) => {
 export const signUp = (username, email, password, confirmPassword) => {
   return async (dispatch) => {
     // Validation
-    if (username === "" || email === "" || password === "" || confirmPassword === "" ) {
+    if (!isValidRequiredInput(username, email, password, confirmPassword)) {
       alert("必須項目が未入力です");
       return false;
     }
 
     if (password !== confirmPassword) {
       alert("パスワードが一致していません。");
+      return false;
+    }
+
+    if(!isValidEmailFormat(email)) {
+      alert("メールアドレスの形式が不正です。もう一度お試しください。");
       return false;
     }
 
