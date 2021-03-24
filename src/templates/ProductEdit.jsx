72c245879546744ce702e0fb0a595941d229cc34
yuki sawabe <sawabe0v0@gmail.com>
2021-03-25 07:59:17 +0900

    add SelectBox to ProductEdit component

--- src/templates/ProductEdit.jsx
+++ src/templates/ProductEdit.jsx
@@ -1,10 +1,14 @@
 import React, { useState, useCallback } from "react";
 import TextInput from "../components/UIkit/TextInput";
+import SelectBox from "../components/UIkit/SelectBox";
+import PrimaryButton from "../components/UIkit/PrimaryButton";
 
 const ProductEdit = () => {
   const [name, setName] = useState(""),
         [description, setDescription] = useState(""),
-        [price, setPrice] = useState("");
+        [price, setPrice] = useState(""),
+        [category, setCategory] = useState(""),
+        [gender, setGender] = useState("");
 
   const inputName = useCallback((event) => {
     setName(event.target.value)
@@ -15,23 +19,47 @@ const ProductEdit = () => {
   const inputPrice = useCallback((event) => {
     setPrice(event.target.value)
   }, [setPrice]);
+
+  const categories = [
+    { id: "tops",   name: "トップス" },
+    { id: "shirts", name: "シャツ" },
+    { id: "pants",  name: "パンツ" }
+  ];
+
+  const genders = [
+    { id: "all", name: "すべて"},
+    { id: "male", name: "メンズ"},
+    { id: "femail", name: "レディース"}
+  ];
   
   return (
    <section>
      <h2 className="u-text__headline u-text-center">商品の登録・編集</h2>
      <div className="c-section-container">
       <TextInput
-        fullwidth={true} label={"商品名"} multiline={false} required={true}
+        fullWidth={true} label={"商品名"} multiline={false} required={true}
         onChange={inputName} rows={1} value={name} type={"text"}
       />
       <TextInput
-        fullwidth={true} label={"商品説明"} multiline={true} required={true}
+        fullWidth={true} label={"商品説明"} multiline={true} required={true}
         onChange={inputDescription} rows={5} value={description} type={"text"}
       />
+      <SelectBox
+        label={"カテゴリー"} required={true} options={categories} select={setCategory} value={category}
+      />
+      <SelectBox
+        label={"性別"} required={true} options={genders} select={setGender} value={gender}
+      />
       <TextInput
-        fullwidth={true} label={"商品名"} multiline={false} required={true}
+        fullWidth={true} label={"価格"} multiline={false} required={true}
         onChange={inputPrice} rows={1} value={price} type={"number"}
       />
+      <div className="module-spacer--medium" />
+      <div classNmae="center">
+        <PrimaryButton
+          label={"商品情報を保存"}
+        />
+      </div>
      </div>
    </section> 
   );
