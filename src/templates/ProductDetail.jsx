4eb79a799f38afeb5c7b899689366ff16d4b176d
yuki sawabe <sawabe0v0@gmail.com>
2021-04-18 08:12:56 +0900

    add ImageSwiper to ProductDetail

--- src/templates/ProductDetail.jsx
+++ src/templates/ProductDetail.jsx
@@ -3,6 +3,7 @@ import { useSelector } from "react-redux";
 import { db } from "../firebase";
 import { makeStyles } from "@material-ui/styles";
 import HTMLReactParser from "html-react-parser";
+import { ImageSwiper } from "../components/Products/ImageSwiper";
 
 const useStyles = makeStyles((theme) => ({
   sliderBox: {
@@ -64,7 +65,7 @@ const ProductDetail = () => {
       {product && (
         <div className="p-grid__row">
           <div className={classes.sliderBox}>
-
+            <ImageSwiper images={product.images} />
           </div>
           <div className={classes.detail}>
             <h2 className="u-text__headline">{product.name}</h2>
