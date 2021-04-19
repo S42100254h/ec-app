ed6cd0695ad75485faff99937086591bc47d9453
yuki sawabe <sawabe0v0@gmail.com>
2021-04-20 07:52:03 +0900

    fix bug for swiper

--- src/components/Products/imageSwiper.jsx
+++ src/components/Products/imageSwiper.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import Swiper from "react-id-swiper";
 import NoImage from "../../assets/img/src/no_image.png";
+import 'swiper/css/swiper.css';
 
 const ImageSwiper = (props) => {
   const [params] = useState({
@@ -14,7 +15,8 @@ const ImageSwiper = (props) => {
       nextEl: ".swiper-button-next",
       prevEl: ".swiper-button-prev"
     },
-    loop: true
+    loop: true,
+    spaceBetween: 30
   });
   const images = props.images;
 
@@ -26,7 +28,7 @@ const ImageSwiper = (props) => {
         </div>
       ) : (
         images.map(image => (
-          <div className="p-media__thumb">
+          <div className="p-media__thumb" key={image.id}>
             <img src={image.path} alt="商品画像" />
           </div>
         ))
