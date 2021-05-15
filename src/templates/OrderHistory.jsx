403f9ff8b172fe4c94c17c9a88d3b9e6853ef11a
yuki sawabe <sawabe0v0@gmail.com>
2021-05-15 13:17:20 +0900

    create basis of OrderHistory component

---
+++ src/templates/OrderHistory.jsx
@@ -0,0 +1,32 @@
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import List from "@material-ui/core/List";
+import { makeStyles } from "@material-ui/styles";
+
+const useStyles = makeStyles((theme) => ({
+  orderList: {
+    background: theme.palette.grey["100"],
+    margin: "0 auto",
+    padding: 32,
+    [theme.breakpoints.down("md")]: {
+      width: "100%"
+    },
+    [theme.breakpoints.up("md")]: {
+      width: 760
+    }
+  }
+}));
+
+const OrderHistory = () => {
+  const classes = useStyles();
+
+  return (
+    <section className="e-section-wrapin">
+      <List className={classes.orderList}>
+
+      </List>
+    </section>
+  );
+};
+
+export default OrderHistory;
